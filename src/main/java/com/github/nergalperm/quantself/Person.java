832f22578ac161f573aa71b6b23ea867f36996a3
Евгений Терехов <nergal@psu.ru>
2025-01-13 09:34:48 +0400

    feat: sample application connecting to Neo4j DB

---
+++ src/main/java/com/github/nergalperm/quantself/Person.java
@@ -0,0 +1,54 @@
+package com.github.nergalperm.quantself;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+@Node
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private Person() {
+        // Empty constructor required as of Neo4j API 2.0.5
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Relationship(type = "TEAMMATE")
+    public Set<Person> teammates;
+
+    public void worksWith(Person person) {
+        if (teammates == null) {
+            teammates = new HashSet<>();
+        }
+        teammates.add(person);
+    }
+
+    public String toString() {
+
+        return this.name + "'s teammates => "
+            + Optional.ofNullable(this.teammates).orElse(
+                Collections.emptySet()).stream()
+            .map(Person::getName)
+            .toList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
