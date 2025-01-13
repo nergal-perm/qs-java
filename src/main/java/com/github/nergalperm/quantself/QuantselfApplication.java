5457c996011482e838bb5ec2e2ca845206d04f92
Евгений Терехов <nergal@psu.ru>
2025-01-13 08:29:55 +0400

    initialized spring project

---
+++ src/main/java/com/github/nergalperm/quantself/QuantselfApplication.java
@@ -0,0 +1,13 @@
+package com.github.nergalperm.quantself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantselfApplication.class, args);
+    }
+
+}
